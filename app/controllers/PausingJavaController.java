e1bff46e1c056a062017bfe69e6cdc1a4786417b
retroryan <ryan@grandcloud.com>
2013-03-07 23:15:30 -0700

    modifying tests to strain memory and work with gatling.

--- app/controllers/PausingJavaController.java
+++ app/controllers/PausingJavaController.java
@@ -1,20 +1,39 @@
 package controllers;
 
 import play.Logger;
+import play.libs.Akka;
 import play.libs.F;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
 
 
 public class PausingJavaController extends Controller {
 
-    public static Result pause(long duration) {
-        Logger.debug("java returning " + duration);
-        F.Promise<Result> timeout = F.Promise.timeout((Result) ok(String.valueOf(duration)), duration);
-        return async(timeout);
+    public static Result pause(final long duration) {
+        Logger.debug("java pausing for  " + duration + " seconds");
+
+        Callable<Long> callable = new Callable<Long>() {
+            public Long call() {
+                return duration;
+            }
+        };
+
+        F.Promise<Long> promiseOfInt = Akka.timeout(callable,duration, TimeUnit.SECONDS);
+
+        return async(
+                promiseOfInt.map(
+                        new F.Function<Long, Result>() {
+                            public Result apply(Long i) {
+                                return ok(i.toString());
+                            }
+                        }
+                )
+        );
     }
 
     public static String memoryOverload(int memoryFillSize) {
